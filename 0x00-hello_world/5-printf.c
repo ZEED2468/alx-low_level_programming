9561f44f97bbf999b7c15287347a63f4f8389e30
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-08 16:23:52 -0700

    Print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function 
+* Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
-{ 
+{
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-:%s/\s\+$//e
+
