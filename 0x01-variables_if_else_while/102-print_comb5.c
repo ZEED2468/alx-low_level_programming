f00f80dfc9fbabe5575bc20b23781343c93de599
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-09 11:03:58 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - print a num pair from 0-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0.
- */
+ *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ *Return: Always 0 (Success)
+*/
 
 int main(void)
 {
