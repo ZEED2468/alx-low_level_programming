4f02f7d67c53361ea91edd564a47a7c6858d10e0
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-09 11:55:57 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
-*/
+ * main - print a num pair range from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -16,18 +16,17 @@ int main(void)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
+			for (t = tens; t <= '9'; t++) /*print second of pair*/
 			{
-				for (o = ones + 1; o <= '9'; o++)
+				for(o = ones + 1; o <= '9'; o++)
 				{
 					putchar(tens);
 					putchar(ones);
 					putchar(' ');
 					putchar(t);
 					putchar(o);
-
 					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
+								(t == '9' && o == '9')))
 					{
 						putchar(',');
 						putchar(' ');
