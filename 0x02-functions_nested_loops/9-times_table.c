574e861c0bce60e7dab36b3d70ccede01414aad9
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-13 16:50:16 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,14 +3,14 @@
 /**
  * times_table - prints the times table from 0 - 9.
  * Return: Nothing.
- */
+*/
 
 void times_table(void)
 
 {
 	int i, j, res;
 
-	for (i = 0; i <= 9; i++)	
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
