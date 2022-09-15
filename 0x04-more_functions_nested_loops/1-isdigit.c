ce747f065458382725543d877d2fe99b7f7a2d12
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-15 15:27:46 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for a digit
+ * @c : character to check
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+
+}
