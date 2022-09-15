217280d1ee3c68f8543a2e349c41867c0aee7d84
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-15 15:45:06 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 ten times
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	char i;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
