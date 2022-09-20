097e37d496dc71816ed3c34e2cd5d22da6c412f3
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-19 17:20:16 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints to string in reverse
+ * @str: Strin to print
+ * Return: Nothing
+ */
+
+void print_rev(char *str)
+
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+
+}
