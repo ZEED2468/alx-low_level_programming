c829eed8afe24a805bc706c1c136b376a637d721
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-22 04:56:02 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,32 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _strncat - concatenate n bytes to destination string
- * @dest: string to be appended to
- * @src: string to append
- * @n: append n number of bytes(chars)
- * Return: concatenated string
+ * _strncat - concatenates n bytes of two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number of bytes
+ * Return: void
  */
 
 char *_strncat(char *dest, char *src, int n)
+
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (dest[i] != '\0') /*i is index of null terminator*/
+	i = 0;
+	while (dest[i] != '\0')
+	{
 		i++;
+	}
 
-	while (src[j] != src[n]) /*append replacing null terminator*/
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
-		i++;
 		j++;
+		i++;
 	}
+
 	dest[i] = '\0';
 
 	return (dest);
