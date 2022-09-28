f64c13fa940adf18011f1da0d40b0e23c04995d0
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-28 16:33:38 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,3 +17,17 @@ int find_root(int n, int root)
 
 	return (find_root(n, root + 1));
 }
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ * Return: natural square root, or -1 if not natural root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (find_root(n, 0));
+}
