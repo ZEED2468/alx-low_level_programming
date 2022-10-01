453f75651706b9efd9ce6b776ff01619e45f50d3
ZEDD2468 <victoradebayo360@gmail.com>
2022-09-30 19:54:41 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,9 +10,10 @@
  * Return: If the number of arguments is not exactly one - 1.
  *         Otherwise - 0.
  */
+
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int c, coins = 0;
 
 	if (argc != 2)
 	{
@@ -20,35 +21,33 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	cents = atoi(argv[1]);
+	c = atoi(argv[1]);
 
-	while (cents > 0)
+	while (c > 0)
 	{
 		coins++;
-		if ((cents - 25) >= 0)
+		if ((c - 25) >= 0)
 		{
-			cents -= 25;
+			c -= 25;
 			continue;
 		}
-		if ((cents - 10) >= 0)
+		if ((c - 10) >= 0)
 		{
-			cents -= 10;
+			c -= 10;
 			continue;
 		}
-		if ((cents - 5) >= 0)
+		if ((c - 5) >= 0)
 		{
-			cents -= 5;
+			c-= 5;
 			continue;
 		}
-		if ((cents - 2) >= 0)
+		if ((c - 2) >= 0)
 		{
 			cents -= 2;
 			continue;
 		}
-		cents--;
+		c--;
 	}
-
 	printf("%d\n", coins);
-
 	return (0);
 }
