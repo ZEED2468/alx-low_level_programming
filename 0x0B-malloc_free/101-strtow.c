f79ce3ae4210c13623b5d1daf7ed52681ba1ff54
ZEDD2468 <victoradebayo360@gmail.com>
2022-10-04 17:45:59 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,34 +7,6 @@
  * Return: number of words
  */
 
-int count_word(char *s)
-{
-	int flag, c, w;
-
-	flag = 0;
-	w = 0;
-
-	for (c = 0; s[c] != '\0'; c++)
-	{
-		if (s[c] == ' ')
-			flag = 0;
-		else if (flag == 0)
-		{
-			flag = 1;
-			w++;
-		}
-	}
-
-	return (w);
-}
-
-/**
- * **strtow - splits a string into words
- * @str: string to split
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
- */
-
 char **strtow(char *str)
 {
 	char **matrix, *tmp;
