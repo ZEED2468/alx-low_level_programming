89fc37eea898b7a56d104a6d1504e1cffc839313
ZEDD2468 <victoradebayo360@gmail.com>
2022-10-10 17:59:17 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,11 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
+/**
+ *
+ * File: 0-object_like_macro.h
+ * Desc: as an abbreviation for the token 1024
+ */
 #define SIZE 1024
 
 #endif
