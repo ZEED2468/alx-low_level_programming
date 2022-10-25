c1b626aa9ba99af107567fcf42fc7575e5feb5d5
ZEDD2468 <victoradebayo360@gmail.com>
2022-10-25 07:23:25 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,46 +1,27 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * delete_nodeint_at_index - deletes a node at a given position.
- * @head: head of linked list
- * @index: index to delete
- * Return: 1 if it succeeded, -1 if it failed
+ * *reverse_listint - function to reverse order of nodes
+ * @head:  pointer to head
+ * Return: always successful
  */
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *current;
-	listint_t *tmp;
+	listint_t *forward;
+	listint_t *back = NULL;
 
-	if (head == NULL || (*head) == NULL)
-		return (-1);
-
-	current = *head;
-	if (index == 0)
+	if (head  == NULL)
+		return (NULL);
+	forward = *head;
+	while (forward != NULL)
 	{
-		if ((*head)->next)
-			(*head) = (*head)->next;
-		else
-			(*head) = NULL;
-		free(current);
-		return (1);
+		forward = forward->next;
+		forward = (*head)->next;
+		(*head)->next = back;
+		back = *head;
+		*head = forward;
 	}
-
-	while (index != 1)
-	{
-		if (current->next == NULL)
-			return (-1);
-
-		current = current->next;
-		--index;
-	}
-	tmp = current->next;
-	if (current->next->next)
-		current->next = current->next->next;
-	else
-		current->next = NULL;
-	free(tmp);
-
-	return (1);
+	*head = back;
+	return (*head);
 }
